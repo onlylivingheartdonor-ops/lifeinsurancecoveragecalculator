607e24e1837d65ce739782976d76f71c03046c40
hsking1964 <onlylivingbraindonor@gmail.com>
2026-05-07 18:31:28 -0400

    update

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,8 @@ export default function RootLayout({ children }) {
           content="Estimate how much life insurance coverage you may need based on income, dependents, debts, and savings."
         />
 
-        https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3475627763908800
+       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3475627763908800"
+     crossorigin="anonymous"></script>
       </head>
       <body>{children}</body>
     </html>
